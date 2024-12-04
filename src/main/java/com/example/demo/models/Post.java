ea2b96bfba32c4a19131a9c952eb1814c8803843
NikolayLyapin <l.nick84@mail.ru>
2024-12-04 21:13:12 +0400

    first commit

---
+++ src/main/java/com/example/demo/models/Post.java
@@ -0,0 +1,34 @@
+package com.example.demo.models;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@Table(name = "blog_t")
+@Entity
+@AllArgsConstructor
+
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "anons")
+    private String anons;
+    @Column(name = "full_text")
+    private String full_text;
+    @Column(name = "views")
+    private int views;
+
+    public Post() {
+
+    }
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
+}
